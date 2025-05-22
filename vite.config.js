0ad77473f933abddcc51551b41740ac8745dab14
dansodre <eng.danilosodre@gmail.com>
2025-05-22 02:14:41 -0300

    Configura projeto Vite para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/Revitlegis-V2/', // <<-- ADICIONE ESTA LINHA! Use o nome exato do seu repositório.
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
